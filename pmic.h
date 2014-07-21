91b16d9730ae5ad52a0641f9759fded32a76c1d0
gsatish100 <gsatish100@gmail.com>
2014-07-21 18:42:40 +0530

    Create pmic.h
    pmic.h

---
+++ pmic.h
@@ -0,0 +1,10 @@
+File path: board/ti/am335x/pmic.h
+File name:pmic.h
+
+
+ #define PMIC_OP_REG_SEL_MASK			(0x7F)
+ #define PMIC_OP_REG_SEL_1_1_3			(0x2E)	/* 1.1375 V */
+ #define PMIC_OP_REG_SEL_1_2_6			(0x38)	/* 1.2625 V */
++#define PMIC_OP_REG_SEL_1_3_2_5			(0x3D)	/* 1.3250 V */
+
+
